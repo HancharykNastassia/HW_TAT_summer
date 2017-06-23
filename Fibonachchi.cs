0eeb0efe400e63f9172121a593dfc8f77d9f6db5
Nastassia Hancharyk <goncharik.anastasia@mail.ru>
2017-06-24 00:15:09 +0300

    the ask for reenter

--- Fibonachchi.cs
+++ Fibonachchi.cs
@@ -4,42 +4,44 @@ namespace Fibonachchi
 {
     class FibonachchiMain
     {
-        static void Main (string [] args)
+        static void Main(string[] args)
         {
+            Console.WriteLine("Enter number");
+            int number = 0;
             try
             {
-                Console.WriteLine("Enter number");
-                int number = int.Parse(Console.ReadLine());
-                bool isFibonacci = false;
-                if ((number == 0) || (number == 1))
+                number = int.Parse(Console.ReadLine());
+            }
+            catch (System.FormatException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("Enter a number please.");
+                number = int.Parse(Console.ReadLine());
+            }
+            bool isFibonacci = false;
+            if ((number == 0) || (number == 1))
+            {
+                Console.WriteLine(number.ToString() + " is in the Fibonacci sequence");
+            }
+            else
+            {
+                int[] fibonacciNumber = new int[] { 0, 1, 1 };
+
+                while (fibonacciNumber[2] < number)
+                {
+                    fibonacciNumber[0] = fibonacciNumber[1];
+                    fibonacciNumber[1] = fibonacciNumber[2];
+                    fibonacciNumber[2] = fibonacciNumber[0] + fibonacciNumber[1];
+                    isFibonacci = (fibonacciNumber[2] == number);
+                }
+                if (isFibonacci)
                 {
                     Console.WriteLine(number.ToString() + " is in the Fibonacci sequence");
                 }
                 else
                 {
-                    int[] fibonacciNumber = new int[] { 0, 1, 1 };
-
-                    while (fibonacciNumber[2] < number)
-                    {
-                        fibonacciNumber[0] = fibonacciNumber[1];
-                        fibonacciNumber[1] = fibonacciNumber[2];
-                        fibonacciNumber[2] = fibonacciNumber[0] + fibonacciNumber[1];
-                        isFibonacci = (fibonacciNumber[2] == number);
-                    }
-                    if (isFibonacci)
-                    {
-                        Console.WriteLine(number.ToString() + " is in the Fibonacci sequence");
-                    }
-                    else
-                    {
-                        Console.WriteLine(number.ToString() + " is not in the Fibonacci sequence");
-                    }
-                }                
-            }
-            catch (System.FormatException ex)
-            {
-                Console.WriteLine("Error: " + ex.Message);
-                Console.WriteLine("Enter a number please.")
+                    Console.WriteLine(number.ToString() + " is not in the Fibonacci sequence");
+                }
             }
             Console.WriteLine("Press any key for exit");
             Console.ReadKey();
