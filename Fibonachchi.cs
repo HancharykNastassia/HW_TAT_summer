3086ca64a8f4e5eb35ae232bbf751f97f42cbfb6
Nastassia Hancharyk <goncharik.anastasia@mail.ru>
2017-06-23 19:43:47 +0300

    Added message for exit

--- Fibonachchi.cs
+++ Fibonachchi.cs
@@ -11,13 +11,11 @@ namespace Fibonachchi
                 Console.WriteLine("Введите число");
                 int number = int.Parse(Console.ReadLine());
                 bool f = false;
-
                 if (number == 0 || number == 1)
                 Console.WriteLine(number.ToString() + " - число из последовательности Фибоначчи");
                 else
                 {
                     int[] fib = new int[] { 0, 1, 1 };
-
                     while (fib[2] < number)
                     {
                         fib[0] = fib[1];
@@ -31,7 +29,7 @@ namespace Fibonachchi
             }
             catch (System.FormatException ex)
             {
-                Console.WriteLine("Неверный формат");
+                Console.WriteLine("Ошибка: " + ex.Message);
             }
             Console.ReadKey();
         }
