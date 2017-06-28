6814a8733a7c5c3ca0a2cb352d87f54832d3bf3e
Nastassia Hancharyk <goncharik.anastasia@mail.ru>
2017-06-28 14:15:03 +0300

    Last changes pert 1

---
+++ MegaSolution/Sequance/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sequence
+{
+    class EntryPoint
+    {        
+        static void Main(string[] args)
+        {
+            IsSequenceUndecrease enterSequense = new IsSequenceUndecrease();
+            int[] sequence;
+            do
+            {
+                sequence = enterSequense.EnterSequense();
+            }
+            while (sequence == null);
+            bool isUndecrease = enterSequense.isUndecreasing(sequence);
+            string positiveMessage = "The sequence is undecreasing";
+            string negativeMessage = "The sequence is not undecreasing";
+            Console.WriteLine(isUndecrease ? positiveMessage : negativeMessage);
+            Console.WriteLine("Press any key for exit");
+            Console.ReadKey();
+        }
+    }
+}
