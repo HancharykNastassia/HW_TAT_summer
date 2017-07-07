99d50a5f921dfad05093e573b15b38a270f3ba21
Nastassia Hancharyk <goncharik.anastasia@mail.ru>
2017-07-07 15:15:24 +0300

    1) Changed InputClass with SequenceInputer 2) Added method for parse inti int 3) Added a name to the magic number 4) Corrected the grammer mistake

--- MegaSolution/Sequance/EntryPoint.cs
+++ MegaSolution/Sequance/EntryPoint.cs
@@ -6,21 +6,46 @@ namespace Sequence
     {
         static void Main(string[] args)
         {
-            var enterSequence = new InputClass();
+            const string NOTASEQUENCEMESSAGE = "One number is not a sequence. Please enter a sequence.";
+            const string POSITIVEMESSAGE = "The sequence is undecreasing";
+            const string NEGATIVEMESSAGE = "The sequence is not undecreasing";
+            var enterSequence = new SequenceInputer();
             var checkSequence = new CheckSequence();
             int[] sequence;
             bool isUndecrease = true;
-            const string NOTASEQUENCEMESSAGE = "One number is not a sequence. Please enter a sequence.";
-            sequence = enterSequence.EnterSequense();
-            while (sequence.Length == 1)
+            const int minSequenceLength = 2;
+
+            enterSequence.SuccessInput = false;
+            while (!enterSequence.SuccessInput)
             {
-                Console.WriteLine(NOTASEQUENCEMESSAGE);
-                sequence = enterSequence.EnterSequense();
+                try
+                {
+                    sequence = enterSequence.ParseNubmersOfSequence(enterSequence.EnterSequense());
+                    while (sequence.Length < minSequenceLength)
+                    {
+                        Console.WriteLine(NOTASEQUENCEMESSAGE);
+                        sequence = enterSequence.ParseNubmersOfSequence(enterSequence.EnterSequense());
+                    }
+                    enterSequence.SuccessInput = true;
+                    isUndecrease = checkSequence.IsUndecreasing(sequence);
+                    Console.WriteLine(isUndecrease ? POSITIVEMESSAGE : NEGATIVEMESSAGE);
+                }
+                catch (System.FormatException ex)
+                {
+                    Console.WriteLine("ERROR " + ex.Message);
+                    enterSequence.SuccessInput = false;
+                }
+                catch (System.OverflowException ex)
+                {
+                    Console.WriteLine("ERROR " + ex.Message);
+                    enterSequence.SuccessInput = false;
+                }
+                catch (System.NullReferenceException ex)
+                {
+                    Console.WriteLine("ERROR " + ex.Message);
+                    enterSequence.SuccessInput = false;
+                }
             }
-            isUndecrease = checkSequence.IsUndecreasing(sequence);
-            const string POSITIVEMESSAGE = "The sequence is undecreasing";
-            const string NEGATIVEMESSAGE = "The sequence is not undecreasing";
-            Console.WriteLine(isUndecrease ? POSITIVEMESSAGE : NEGATIVEMESSAGE);
             Console.WriteLine("Press any key for exit");
             Console.ReadKey();
         }
--- MegaSolution/Sequance/Inputer.cs
+++ MegaSolution/Sequance/Inputer.cs
@@ -1,44 +1,32 @@
 ﻿using System;
 
-using System;
-
 namespace Sequence
 {
-    class InputClass
+    class SequenceInputer
     {
         const string INSTRUCTIONS1LINE = "Enter a sequance of nubers. ";
         const string INSTRUCTIONS2LINE = "Use Space to devide numbers. Use Enter to finish input.";
-        public int[] EnterSequense()
+        bool successInput;
+        public bool SuccessInput
         {
-            bool successInput = false;
-            int[] sequanceNumbers = { 0 };
-            while (!successInput)
+            get { return successInput; }
+            set { successInput = value; }
+        }
+        public string[] EnterSequense()
+        {
+            Console.WriteLine(INSTRUCTIONS1LINE);
+            Console.WriteLine(INSTRUCTIONS2LINE);
+            string[] inputNumbers = Console.ReadLine().Split();
+            return inputNumbers;
+        }
+        public int[] ParseNubmersOfSequence (String[] inputNumbers)
+        {
+            int[] sequenceNumbers = new int[inputNumbers.Length];
+            for (int i = 0; i < inputNumbers.Length; i++)
             {
-                successInput = true;
-                Console.WriteLine(INSTRUCTIONS1LINE);
-                Console.WriteLine(INSTRUCTIONS2LINE);
-                string[] inputNumbers;
-                try
-                {
-                    inputNumbers = Console.ReadLine().Split();
-                    sequanceNumbers = new int[inputNumbers.Length];
-                    for (int i = 0; i < inputNumbers.Length; i++)
-                    {
-                        sequanceNumbers[i] = int.Parse(inputNumbers[i]);
-                    }
-                }
-                catch (System.FormatException ex)
-                {
-                    Console.WriteLine("Error " + ex.Message);
-                    successInput = false;
-                }
-                catch (System.OverflowException ex)
-                {
-                    Console.WriteLine("Error " + ex.Message);
-                    successInput = false;
-                }
+               sequenceNumbers[i] = int.Parse(inputNumbers[i]);
             }
-            return sequanceNumbers;
+            return sequenceNumbers;
         }
     }
 }
