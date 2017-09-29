2af1cf878297898ebcd8493ae5b921ee261717d2
HancharykNastassia <goncharik.anastasia@mail.ru>
2017-09-29 20:35:53 +0300

    Deleting unneccessary files

--- MegaSolution/CreatingLineFromRandomPartsOfOthers/NewLineCreation.cs
+++ MegaSolution/CreatingLineFromRandomPartsOfOthers/NewLineCreation.cs
@@ -10,27 +10,29 @@ namespace CreatingLineFromRandomPartsOfOthers
       Random rand = new Random();
       position = rand.Next(line[1].Length - 1);
       length = rand.Next(1, line[1].Length - position);
-      LineCutter lineCutter = new LineCutter();
-      char[] lineIn = lineCutter.CutLineN2(line[1], position, length);
+      char[] lineIn = new char [length];
+      Array.Copy(line[1].ToCharArray(), position, lineIn, 0, length);
       position = rand.Next(line[0].Length - 1);
       length = rand.Next(1, line[0].Length - position);
       string resultLine = String.Empty;
       if (position != 0)
       {
-        char[] beginning = lineCutter.CutLineN1Beginning(line[0], position);
-        for (int i = 0; i < beginning.Length; i++)
+        char[] beginning = new char [position];
+        Array.Copy(line[0].ToCharArray(), beginning, position);
+        foreach (int i in beginning)
         {
           resultLine += beginning[i];
         }
       }
-      for (int i = 0; i < lineIn.Length; i++)
+      foreach (int i in lineIn)
       {
         resultLine += lineIn[i];
       }
       if (position != (line[0].Length - 1))
       {
-        char[] ending = lineCutter.CutLineN1Ending(line[0], position, length);
-        for (int i = 0; i < ending.Length; i++)
+        char[] ending = new char[line[0].Length - position - length];
+        Array.Copy(line[0].ToCharArray(), position + length, ending, 0, line[0].Length - position - length);
+        foreach (int i in ending)
         {
           resultLine += ending[i];
         }
