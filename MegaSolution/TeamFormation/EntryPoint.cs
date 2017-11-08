fe3cd44e2a5b54f24c171adc1dc61013871b620c
HancharykNastassia <goncharik.anastasia@mail.ru>
2017-11-08 12:30:51 +0300

    Statred project

---
+++ MegaSolution/TeamFormation/EntryPoint.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TeamFormation
+{
+  class EntryPoint
+  {
+    const string FILENAME = "";
+    const string FOREXIT = "Press any key for exit";
+    static void Main(string[] args)
+    {
+      try
+      {
+        List<Developer> listOfAvailableDevelopers = new List<Developer>();
+        using (StreamReader readThreat = File.OpenText(FILENAME))
+        {
+          while (!readThreat.EndOfStream)
+          {
+            string line = readThreat.ReadLine();
+            if (readThreat.EndOfStream)
+            {
+              break;
+            }
+            string[] partOfInputLine = line.Split('|');
+            int productivity = Int32.Parse(partOfInputLine[1]);
+            int salary = Int32.Parse(partOfInputLine[2]);
+            listOfAvailableDevelopers.Add(new Developer(productivity, salary, partOfInputLine[0]));
+          }
+        }
+        Console.WriteLine("Input the amount of money");
+        int nesessaryAmount = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Input nessessary productivity");
+        int nesessaryProductivity = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Choose the criteria");
+        int criteriaNumber = Int32.Parse(Console.ReadLine());
+        Mediator mediator = new Mediator(criteriaNumber, nesessaryAmount, nesessaryProductivity, 
+                                         listOfAvailableDevelopers);
+        mediator.MakeTeam();
+        List<Developer> team = mediator.Team;
+        StringBuilder output = new StringBuilder("Team: " + team.Capacity);
+      }
+      finally
+      {
+        Console.WriteLine(FOREXIT);
+        Console.ReadKey();
+      }
+    }
+  }
+}
---
+++ MegaSolution/TeamFormation/Mediator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeamFormation
+{
+  class Mediator
+  {
+    int criteria;
+    int amount;
+    int productivity;
+    List<Developer> listOfDevelopers;
+    List<Developer> team;
+    public List<Developer> Team
+    {
+      get => team;
+    }
+    public Mediator(int criteria, int amount, int productivity, List<Developer> list)
+    {
+      this.criteria = criteria;
+      this.amount = amount;
+      this.productivity = productivity;
+      listOfDevelopers = list;
+    }
+    public void MakeTeam()
+    {
+      switch (criteria)
+      {
+        case 1:
+          {
+            MakeTeamOfMaximumProductivityInFixedAmount();
+            break;
+          }
+        case 2:
+          {
+            break;
+          }
+        case 3:
+          {
+            break;
+          }
+        default: throw new Exception();
+      }
+    }
+    private void MakeTeamOfMaximumProductivityInFixedAmount()
+    {
+      int checkSumOfSalary = 0;
+      foreach(var developer in listOfDevelopers)
+      {
+        checkSumOfSalary += developer.Salary;
+      }
+      if (checkSumOfSalary == amount)
+      {
+        team = listOfDevelopers;
+      }
+      if (checkSumOfSalary > amount)
+      {
+
+      }
+    }
+  }
+}
