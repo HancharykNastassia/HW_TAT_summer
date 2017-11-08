f51ba3127830be8dc758b5db78e67fae9a3d73a0
HancharykNastassia <goncharik.anastasia@mail.ru>
2017-11-08 16:02:12 +0300

    Writing 3 methods in Mediator

--- MegaSolution/TeamFormation/Mediator.cs
+++ MegaSolution/TeamFormation/Mediator.cs
@@ -32,10 +32,12 @@ namespace TeamFormation
           }
         case 2:
           {
+            MakeTeamOfMinimumAmountInFixedProductivity();
             break;
           }
         case 3:
           {
+            MakeTeamOfMinimumDevelopersInFixedProductivityAcceptJunior();
             break;
           }
         default: throw new Exception();
@@ -86,6 +88,126 @@ namespace TeamFormation
           if (sumProductivity > maxProductivity)
           {
             maxProductivity = sumProductivity;
+            team.Clear();
+            foreach (var developer in tempList)
+            {
+              team.Add(developer);
+            }
+          }
+        }
+        tempList.Clear();
+      }
+    }
+    private void MakeTeamOfMinimumAmountInFixedProductivity()
+    {
+      int checkSumOfProductivity = 0;
+      int checkSumOfSalary = 0;
+      foreach (var developer in listOfDevelopers)
+      {
+        checkSumOfProductivity += developer.Productivity;
+        checkSumOfSalary += developer.Salary;
+      }
+      if (checkSumOfProductivity == productivity)
+      {
+        foreach (var developer in listOfDevelopers)
+        {
+          team.Add(developer);
+        }
+      }
+      if (checkSumOfProductivity > productivity)
+      {
+        int sumSalary = 0;
+        int sumProductivity = 0;
+        int minSalary = checkSumOfSalary;
+        List<Developer> tempList = new List<Developer>();
+        foreach (var developer in listOfDevelopers)
+        {
+          if (developer.Productivity > productivity)
+          {
+            listOfDevelopers.Remove(developer);
+          }
+        }
+        foreach (var developer in listOfDevelopers)
+        {
+          if (sumProductivity < productivity)
+          {
+            sumSalary += developer.Salary;
+            sumProductivity += developer.Productivity;
+            tempList.Add(developer);
+          }
+          else
+          {
+            break;
+          }
+        }
+        if (sumProductivity == productivity)
+        {
+          if (sumSalary < minSalary)
+          {
+            minSalary = sumSalary;
+            team.Clear();
+            foreach (var developer in tempList)
+            {
+              team.Add(developer);
+            }
+          }
+        }
+        tempList.Clear();
+      }
+    }
+    private void MakeTeamOfMinimumDevelopersInFixedProductivityAcceptJunior()
+    {
+      int checkSumOfProductivity = 0;
+      int checkSumOfSalary = 0;
+      foreach (var developer in listOfDevelopers)
+      {
+        if (developer.Vacancy.Equals("Junior"))
+        {
+          listOfDevelopers.Remove(developer);
+        }
+      }
+      foreach (var developer in listOfDevelopers)
+      {
+        checkSumOfProductivity += developer.Productivity;
+        checkSumOfSalary += developer.Salary;
+      }
+      if (checkSumOfProductivity == productivity)
+      {
+        foreach (var developer in listOfDevelopers)
+        {
+          team.Add(developer);
+        }
+      }
+      if (checkSumOfProductivity > productivity)
+      {
+        int sumProductivity = 0;
+        int minSalary = checkSumOfSalary;
+        int minQuantityOfDevelopers = listOfDevelopers.Capacity; 
+        List<Developer> tempList = new List<Developer>();
+        foreach (var developer in listOfDevelopers)
+        {
+          if (developer.Productivity > productivity)
+          {
+            listOfDevelopers.Remove(developer);
+          }
+        }
+        foreach (var developer in listOfDevelopers)
+        {
+          if (sumProductivity < productivity)
+          {
+            sumProductivity += developer.Productivity;
+            tempList.Add(developer);
+          }
+          else
+          {
+            break;
+          }
+        }
+        if (sumProductivity == productivity)
+        {
+          if (minQuantityOfDevelopers > tempList.Capacity)
+          {
+            minQuantityOfDevelopers = tempList.Capacity;
             team.Clear();
             foreach (var developer in tempList)
             {
